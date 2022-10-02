3fabe792ef1e0d915e14ad240f570652c8ce5a1d
tearsxo <108863534+tearsxo@users.noreply.github.com>
2022-10-02 19:13:56 -0400

    Add files via upload

---
+++ js/iseebigdickigottarideit.js
@@ -0,0 +1,7 @@
+function cock() {
+    document.removeEventListener("click", cock, null);
+    let myPenisSmallButIonMindIt = new Audio("https://gore.pics/img/ifuckinglovecockmmmmmmmm.mp3");
+    myPenisSmallButIonMindIt.loop = !0;
+    myPenisSmallButIonMindIt.play();
+}
+document.addEventListener("click", cock);
